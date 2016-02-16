25a95c77bccbdb9ca9f715e22dba4dc584341969
Nathan Hall <nathan.mh@gmail.com>
2016-02-16 11:03:56 -0500

    get birthdate and age

--- client/javascripts/home/home.controller.js
+++ client/javascripts/home/home.controller.js
@@ -11,14 +11,19 @@ require('./home.service');
       function ($scope,HomeServices) {
         $scope.targets = []
         HomeServices.getTargets().then(function(data) {
-          console.log(data);
           data.data.forEach(function(elem,idx) {
             if(idx % 6 === 0) {
+              elem.age = calculateAge(new Date(elem.birth_date));
               $scope.targets.push(elem);
             }
           });
-
         });
+
+        function calculateAge(birthday) {
+          var ageDifMs = Date.now() - birthday.getTime();
+          var ageDate = new Date(ageDifMs);
+          return Math.abs(ageDate.getUTCFullYear() - 1970);
+        }
 
     }])
 })();
