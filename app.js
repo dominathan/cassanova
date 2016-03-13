1bc3844400a6602aa156548404ec9b996f8f5630
Nathan Hall <nathan.mh@gmail.com>
2016-03-12 19:20:11 -0500

    new relic monitoring

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var newRelic = require('newrelic');
 
 var CronExecutables = require('./services/cronTasks');
 var facebookInfo = require('./services/facebook-login');
