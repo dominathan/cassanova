f8f6d64d7429f650616215877fba133516d483e7
Nathan Hall <nathan.mh@gmail.com>
2016-04-18 21:32:13 -0400

    fix massive fail

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -127,8 +127,8 @@ router.post('/getTinderized', ensureAuthenticated, function(req, res, next) {
                    console.error('Something went wrong, check the status code', updates);
                }
              } else {
-               saveMatches.saveNewMaches(updates.matches,fk_account[0],req.user.id);
-               saveMatches.saveNewMessages(updates,fk_account[0]);
+               saveMatches.saveNewMaches(io,updates.matches,fk_account[0],req.user.id);
+               saveMatches.saveNewMessages(io,updates,fk_account[0]);
                saveMatches.checkBlocks(updates);
              }
            }
