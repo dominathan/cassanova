da77816d7ed4b414ca2490e6c01cfb598de422d6
Nathan Hall <nathan.mh@gmail.com>
2016-04-21 19:14:12 -0400

    adding config for password reset

--- client/javascripts/users/resetPassword.controller.js
+++ client/javascripts/users/resetPassword.controller.js
@@ -3,26 +3,30 @@
 
   angular
     .module('users')
-    .controller('ResetPasswordController', function($scope, UserService, $location, $routeParams) {
+    .controller('ResetPasswordController', function($scope, UserService, $location, $routeParams, Flash) {
 
       $scope.sendEmail = function(user) {
         UserService.sendResetPasswordEmail(user)
         .then(function(success) {
-          console.log(success);
+          var message = `<strong> ${success.data.message} </strong>`
+          Flash.create('success',message,0,{},true);
         })
         .catch(function(err) {
-          console.log(err);
+          var message = `<strong> ${err.data.error} </strong>`
+          Flash.create('danger',message,0,{},true);
         })
       }
 
       $scope.resetPassword = function(user) {
         UserService.resetPassword(user,$routeParams.token)
         .then(function(success) {
-          console.log(success);
-          $location.path('/profile');
+          var message = `<strong> ${success.data.message} </strong>`
+          Flash.create('success',message,0,{},true);
+          $location.path('/login');
         })
         .catch(function(err) {
-          console.log(err);
+          var message = `<strong> ${err.data.message} </strong>`
+          Flash.create('danger',message,0,{},true);
         })
       }
 
---
+++ config/emailConfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+  SENDGRID_KEY: process.env.SENDGRID_KEY,
+  SENDGRID_CLIENT: process.env.SENDGRID_CLIENT
+};
